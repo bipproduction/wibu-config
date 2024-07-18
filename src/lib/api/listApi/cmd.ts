dacd01483d1b2a693292e8a1443b4c5eb4c1297e
bipproduction <bip.production.js@gmail.com>
2024-07-18 12:01:09 +0800

    tambahhan postgres database

--- src/lib/api/listApi/cmd.ts
+++ src/lib/api/listApi/cmd.ts
@@ -2,13 +2,14 @@ import { spawn } from "child_process";
 export async function cmd(req: Request) {
   const { searchParams } = new URL(req.url);
   const cmd = searchParams.get("cmd") || "ls";
-  const result: string = await new Promise((resolve, reject) => {
-    const child = spawn(cmd, { shell: true });
-    child.stdout.on("data", (data) => {
+  console.log(decodeURIComponent(cmd));
+  const result: string = await new Promise((resolve) => {
+    const res = spawn("/bin/bash", ["-c", `"${cmd}"`]);
+    res.stdout.on("data", (data) => {
       resolve(data.toString());
     });
-    child.stderr.on("data", (data) => {
-      reject(data.toString());
+    res.stderr.on("data", (data) => {
+      resolve(data.toString());
     });
   });
   return new Response(result);
